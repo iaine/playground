c15ec0e06c5c5edaee294bdaeb42710b9defd038
iaine <iain.emsley@kellogg.ox.ac.uk>
2018-11-18 21:13:01 +0000

    Initial refactor of the playground

---
+++ sd2/kernel.js
@@ -0,0 +1,25 @@
+/**
+*  Browser data
+*/
+
+var Kernel = {};
+
+Kernel = {
+  constructor: function() {
+    this.data = [];
+    this.mapData = [];
+    this.x = screen.width;
+    this.y = screen.height;
+    this.z = 295;
+    this.commands = [];
+    this.id = Date.now();
+    this.startX = Math.floor(window.innerWidth/2);
+    this.startY = Math.floor(window.innerHeight/2);
+    this.tones = new Map();
+    this.history = [];
+  }, 
+
+  
+};
+
+module.exports = Kernel;
