154f3531da5d3469499f74fbe9ed69f302579e3a
iaine <iain.emsley@kellogg.ox.ac.uk>
2018-11-18 20:30:14 +0000

    Entry point added and update the spatial class

--- sd2/spatial.js
+++ sd2/spatial.js
@@ -2,13 +2,18 @@
 *  Class for mapping to positions on screen  
 */
 
-class Spatial {
+var Spatial;
+
+Spatial = {
   
   /**
   *  Function to map to an (x,y) position
   */
-  var panMap =  function (screenDim, position, minMax) {
+  panMap: function (screenDim, position, minMax) {
     let pos = (position < 0.0) ? -(1-(position/minMax.min)) :  (position/minMax.max);
     return screenDim * pos;
   }
 }
+
+
+module.exports = Spatial;
