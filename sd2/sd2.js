ba7ecaf36bf84b2eeab507a8ba085c1dbbc84ec8
iaine <iain.emsley@kellogg.ox.ac.uk>
2019-01-24 21:24:03 +0000

    Extract main library

--- sd2/sd2.js
+++ sd2/sd2.js
@@ -1,65 +1,46 @@
 /**
 *  Main file to be called
 */
-var SD2 = {};
-
 //set up constants
 const power = Math.pow(2, 1/12);
 
-SD2 = {
-
-  /* Kernel actions */
-  kernel: function(id) {
-    this.data = [];
-    this.mapData = [];
-    this.x = screen.width;
-    this.y = screen.height;
-    this.z = 295;
-    this.commands = [];
-    this.id = id;
-    this.date = Date.now();
-    this.startX = Math.floor(window.innerWidth/2);
-    this.startY = Math.floor(window.innerHeight/2);
-    this.tones = new Map();
-    this.history = [];
-  },
 
   /**
-  *  Function to create a Map of tones to values
+  *  to create a Map of tones to values
   *  Currently limimted to frequencies.
   */
-  toneMap: function(mapKey, mapValue) {
+  var toneMap  = function (mapKey, mapValue) {
     play.tones.set(mapKey, mapValue);
-  },
+  }
   
 
   /*  Music algorithms */
 
   /**
-  *  function to calculate a note frequency
+  *  to calculate a note frequency
   *  https://pages.mtu.edu/~suits/NoteFreqCalcs.html
   */
-  calculateNewNote : function(base, newnote) {
+  var calculateNewNote = function (base, newnote) {
     return base * Math.pow(power, newnote);
-  },
+  }
 
   /**
   *  Calculate Frequency value
   *  f = 2 ^ (d-69)/12 * 440 Hz
   */
-  calculateFrequency : function(midiNote) {
+  var calculateFrequency = function (midiNote) {
     return Math.pow(2, ((midiNote-69)/12)) * 440;
-  },
+  }
 
   /*  Spatial Algorithms */
 
   /**
-  *  Function to map to an (x,y) position
+  *  to map to an (x,y) position
   */
-  panMap: function (screenDim, position, minMax) {
+  var panMap = function (screenDim, position, minMax) {
     let pos = (position < 0.0) ? -(1-(position/minMax.min)) :  (position/minMax.max);
     return screenDim * pos;
-  },
+  }
 
   /* Data functions */
 
@@ -67,7 +48,7 @@ SD2 = {
   *  Generic GET Function
   *
   */
-  getData: function(url, callback) {
+  var getData = function (url, callback) {
 
     function reqListener () {
       play.data = JSON.parse(this.responseText);
@@ -79,15 +60,15 @@ SD2 = {
     oReq.open("GET", url);
     oReq.send();
 
-  },
+  }
 
   /**
-  *  Function to get the minimum and maximum from a value
+  *  to get the minimum and maximum from a value
   *  @param{Array} searchArray - the array to be searched
   *  @param{String} searchKey - the key to be filtered in
   *  @return {Object}
   */
-  getMinMax: function(searchKey) {
+  var getMinMax = function (searchKey) {
 
     let _tmp = [];
 
@@ -96,19 +77,19 @@ SD2 = {
     });
 
     return {'min': Math.min(..._tmp), 'max': Math.max(..._tmp)};
-  },
+  }
 
   /**
-  *  Function to remap the data
+  *  to remap the data
   */
-  mapData: function(keyArray) {
+  var mapData = function (keyArray) {
     let mapData = [];
 
     let _tmp = [];
     play.data.filter(d =>  _tmp.push({"x": d[keyArray[0]], "y":d[keyArray[1]], 'freq': d[keyArray[2]] }));
     play.data = _tmp.slice();
     return play.data;
-  },
+  }
 
   /* Text algorithms */
 
@@ -119,7 +100,7 @@ SD2 = {
   *  Used as it comes in as an object but isn't
   *  an array, so reverse won't work
   */
-  reverse : function(incomingdata) {
+  var reverse = function (incomingdata) {
     if (typeof(incomingdata) == "object") {
       return incomingdata.reverse();
     }
@@ -129,7 +110,3 @@ SD2 = {
      console.log("Cannot reverse " + incomingdata);
     }
   }
-
-}; //end SD2
-
-module.exports = SD2;
